bc66ab21bddc222fee78fd4042d75ef5d6620bd6
buctwbzs <zh_buctwbzs@163.com>
2020-10-27 23:59:56 +0800

    :sparkles: ch07

--- ch07/checkopts.c
+++ ch07/checkopts.c
@@ -1,6 +1,10 @@
 
 #include <netinet/tcp.h>
 #include <netinet/ip.h>
+#ifdef HAVE_NETINET_SCTP_H
+#include <netinet/sctp.h>
+#endif
+#include <netinet6/in6.h>
 #include <sys/socket.h>
 #include <sys/time.h>
 
@@ -26,10 +30,14 @@ struct sock_opts
     int opt_name;
     char *(*opt_val_str)(union val *, int);
 } sock_opts[] = {
+    // 禁止活着开启发送广播消息的能力
     {"SO_BROADCAST", SOL_SOCKET, SO_BROADCAST, sock_str_flag},
+    // 开启debug模式，可使用trpt程序进行检查
     {"SO_DEBUG", SOL_SOCKET, SO_DEBUG, sock_str_flag},
+    // 绕过底层协议的路由机制
     {"SO_DONTROUTE", SOL_SOCKET, SO_DONTROUTE, sock_str_flag},
     {"SO_ERROR", SOL_SOCKET, SO_ERROR, sock_str_int},
+    // 设置存活选项，每两小时发送存活探测分节，可关闭半开连接
     {"SO_KEEPALIVE", SOL_SOCKET, SO_KEEPALIVE, sock_str_flag},
     {"SO_LINGER", SOL_SOCKET, SO_LINGER, sock_str_linger},
     {"SO_OOBINLINE", SOL_SOCKET, SO_OOBINLINE, sock_str_flag},
@@ -43,6 +51,22 @@ struct sock_opts
     {"SO_REUSEPORT", SOL_SOCKET, SO_REUSEPORT, sock_str_flag},
     {"SO_TYPE", SOL_SOCKET, SO_TYPE, sock_str_int},
     {"SO_USELOOPBACK", SOL_SOCKET, SO_USELOOPBACK, sock_str_flag},
+    {"IP_TOS", IPPROTO_IP, IP_TOS, sock_str_int},
+    {"IP_TTL", IPPROTO_IP, IP_TTL, sock_str_int},
+    {"IPV6_DONTFRAG", IPPROTO_IPV6, IPV6_DONTFRAG, sock_str_flag},
+    {"IPV6_UNICAST_HOPS", IPPROTO_IPV6, IPV6_UNICAST_HOPS, sock_str_int},
+    {"IPV6_V6ONLY", IPPROTO_IPV6, IPV6_V6ONLY, sock_str_flag},
+    //  tcp 最大分节
+    {"TCP_MAXSEG", IPPROTO_TCP, TCP_MAXSEG, sock_str_int},
+    //  开启本选项将禁止Nagle算法
+    {"TCP_NODELAY", IPPROTO_TCP, TCP_NODELAY, sock_str_flag},
+#ifdef HAVE_NETINET_SCTP_H
+    {"SCTP_AUTOCLOSE", IPPROTO_SCTP, SCTP_AUTOCLOSE, sock_str_int},
+    {"SCTP_MAXBURST", IPPROTO_SCTP, SCTP_MAXBURST, sock_str_int},
+    {"SCTP_MAXSEG", IPPROTO_SCTP, SCTP_MAXSEG, sock_str_int},
+    {"SCTP_NODEALY", IPPROTO_SCTP, SCTP_NODEALY, sock_str_flag},
+#endif
+
 };
 
 int main(int argc, char const *argv[])
